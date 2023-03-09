9751ee4998f50c36dbfb6971946f793fe7385911
messi618 <54788160+messi618@users.noreply.github.com>
2023-03-08 22:50:40 -0600

    update

---
+++ vivekTradingBot/config.py
@@ -0,0 +1,10 @@
+#Enter robinhood user-name and password:
+username="vk2.vivek.007@gmail.com"
+password="^!((!#$$!)Messi!10"
+
+# to show plot
+plot=True
+
+# set to false to run and execute all buy sell functions
+# set to true until algorithm is complete 
+debug_mode=True
